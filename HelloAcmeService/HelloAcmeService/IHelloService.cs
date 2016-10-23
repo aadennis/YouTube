f120de9f2a9c7d641863ba1b20381377be1d770f
Dennis Wells <dennis.circ@gmail.com>
2016-10-23 20:04:56 +0000

    YouTube WCF demo 1 of 3

---
+++ HelloAcmeService/HelloAcmeService/IHelloService.cs
@@ -0,0 +1,10 @@
+﻿using System.ServiceModel;
+
+namespace HelloAcmeService {
+    [ServiceContract]
+    public interface IHelloService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
---
+++ HelloAcmeService/HelloServiceAClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloServiceAClient {
+    class Program {
+        static void Main(string[] args) {
+            var clientProxy = new ServiceReference1.HelloServiceClient();
+            var msg = clientProxy.GetData(101);
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
